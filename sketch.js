7a22151fa1d004db60ee211788ffba3ec767b91f
ludwigpeking <ludwigpeking@gmail.com>
2023-10-18 16:28:37 +0800

    graph added

--- sketch.js
+++ sketch.js
@@ -11,11 +11,11 @@ let isPaused = false;
 let restartButton, pauseButton, resumeButton, speedSlider;
 let frame = 0;
 const cols = 40;
-const rows = 30;
+const rows = 20;
 const totalTerritory = cols * rows;
 const resolution = 40;
 const noiseScale = 0.2;
-const frameRateSetting = 8;
+const frameRateSetting = 20;
 let bands = [];
 let nr = 0;
 let topLayer, figureLayer;
@@ -36,7 +36,7 @@ function preload() {
 
 function setup() {
     //clear the tiles and bands
-    history = [[],[],[],[],[]]; //population, growth, richness, growthRateChange, age
+    history = [[],[],[],[],[],[]]; //population, growth, richness, growthRateChange, age
     frame = 0;
     bands = [];
     nr = 0;
@@ -110,16 +110,17 @@ function setup() {
   history[2].push(averageRichness);
   history[3].push(0);
   history[4].push("");
+  history[5].push(0);
   ageSustain = 0;
 
 }
 
 function draw() {
-  if (isPaused) {
-    noLoop ();
-}
+ 
   figureLayer.clear();
   topLayer.clear();
+  graphLayer.clear();
+
   let totalRichness = 0;
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
@@ -147,15 +148,16 @@ function draw() {
   frame += 1;
   history[0].push(population);
   let historyLength = 1;
-  let growth = 0;
-  for (let historyPopluations = max(1, frame - 10); historyPopluations <= frame; historyPopluations++) {
-    growth += history[0][historyPopluations] - history[0][historyPopluations-1];
+  let growth = history[0][history[0].length - 1] - history[0][history[0].length - 2];
+  let growthEvenOut = 0;
+  for (let historyPopluations = max(1, frame - 15); historyPopluations <= frame; historyPopluations++) {
+    growthEvenOut += history[0][historyPopluations] - history[0][historyPopluations-1];
     historyLength += 1;
   }
-  growth = growth / historyLength;
+  growthEvenOut = growthEvenOut / historyLength;
 
-  let growthRateChange = growth - history[1][history[1].length - 1];
-  let newAge = getAge(growth, population, averageRichness,growthRateChange);
+  let growthRateChange = growthEvenOut - history[1][history[1].length - 1];
+  let newAge = getAge(growthEvenOut, population, averageRichness,growthRateChange);
   if (newAge === "We Extincted. " + bands.length + " bands lived here.") {
     currentAge = newAge;
     noLoop();
@@ -167,34 +169,36 @@ function draw() {
     ageSustain =0;
   } else if (newAge !== currentAge) {
       ageSustain += 1;
-      if (ageSustain > 5) {
+      if (ageSustain > 10) {
           ageSustain = 0;
           currentAge = newAge;
-          console.log('new age: ', currentAge, "growth:",round (growth,1), "population:",round (population), "averageRichness:",round (averageRichness), "growthRateChange:",round(growthRateChange,2));
+          console.log('new age: ', currentAge, "growth:",round (growthEvenOut,1), "population:",round (population), "averageRichness:",round (averageRichness), "growthRateChange:",round(growthRateChange,2));
           fadeCounter = 355; // Reset fade counter
       }
       
   }
-  history[1].push(growth);
+  history[1].push(growthEvenOut);
   history[2].push(averageRichness);
   history[3].push(growthRateChange);
   history[4].push(currentAge);
+  history[5].push(growth);
   if (fadeCounter > 0 ) {
     fadeCounter -= 8; 
     displayAgeText(currentAge, fadeCounter);
   }
-  updateGraph(population, growth);
-
-  image(topLayer, 0, 0);
-  image(graphLayer, 0, 0);
-  image(figureLayer, 0, 0)
   
+
+  
+ 
+  topLayer.noStroke();
   topLayer.textSize(25);
   topLayer.colorMode(RGB);
   topLayer.textAlign(RIGHT, TOP);
   topLayer.textFont("sans-serif");
   topLayer.fill(255, 200);
-  topLayer.text("growthRateChange = "+round(growthRateChange,1)+", "+currentAge +", richness = " + round(averageRichness)+ ", surviving band(s) =" + count + ";  population = "+ round(population) + "; growth =  "+ round(growth), width - 20, 20);
+  // topLayer.text("growthRateChange = "+round(growthRateChange,1)+", "+currentAge +", richness = " + round(averageRichness)+ ", surviving band(s) =" + count + ";  population = "+ round(population) + "; growth =  "+ round(growthEvenOut), width - 20, 20);
+  topLayer.text(" surviving band(s) =" + count + ";  population = "+ round(population), width - 20, 20);
+
   topLayer.textSize(15);
   topLayer.text("by Qian Li, ludwig.peking@gmail.com, ", width - 100, height - 20);
   topLayer.fill(255, 10, 200);
@@ -203,43 +207,55 @@ function draw() {
   topLayer.textSize(30);
   topLayer.text("a_Malthusian_World@urban_banal.GENESIS(0)", width - 20, height - 50);
 
-  topLayer.stroke(255);
-  topLayer.strokeWeight(2);
-  topLayer.noFill();
-  
+  graphLayer.fill(255,120);
+  graphLayer.noStroke();
+  graphLayer.rect(10,50,410,380);
   // Draw axes
-  topLayer.line(10, graphLayer.height -160, graphLayer.width / 4+10, graphLayer.height -160);  // X-axis
-  topLayer.line(10, graphLayer.height - 10, 10, graphLayer.height-310);  // Y-axis
-  topLayer.rect(10, graphLayer.height - 310, graphLayer.width / 4, 300);  // Graph area
+  graphLayer.stroke(255);
+  graphLayer.strokeWeight(1);
+  graphLayer.noFill();
+  graphLayer.line(10, 230, 410, 230);  // X-axis
+  graphLayer.line(10, 80, 10, 380);  // Y-axis
+  graphLayer.line(210,230, 210,80);
+  // topLayer.rect(10, graphLayer.height - 310, graphLayer.width / 4, 300);  // Graph area
+  updateGraph(history);
 
   image(topLayer,0,0)
+  image(graphLayer, 0, 0);
+  image(figureLayer, 0, 0)
 }
 
-function updateGraph(population, growth) {
-  // graphLayer.clear();  // Clear the previous frame's graph
-  
-  // Map the population and growth to the graph's coordinates
-  let x = map(population, 0, cols * rows * 4, 10, graphLayer.width / 4+10);  // Adjust the divisor to fit the graph on the screen
-  let y = map(growth, -200, 200, graphLayer.height -10, graphLayer.height -310);  // Adjust the divisor and invert y-axis
-  // graphLayer.blendMode(SCREEN );  // Use LIGHTEST if your background is light
-    graphLayer.fill(0, 50);  // Adjust the alpha value as needed
-    graphLayer.noStroke();
-    // graphLayer.rect(0, graphLayer.height - 310, graphLayer.width / 4 + 10, 300);  // Adjust the coordinates and dimensions as needed
-
-   
-
-  graphLayer.stroke(255);
-  graphLayer.strokeWeight(2);
-  graphLayer.noFill();
-  
-  // Draw axes
-  graphLayer.line(10, graphLayer.height -160, graphLayer.width / 4+10, graphLayer.height -160);  // X-axis
-  graphLayer.line(10, graphLayer.height - 10, 10, graphLayer.height-310);  // Y-axis
-
-  graphLayer.fill(255, 0, 0, 100);
+function updateGraph(history) {
+  // for loop to draw all the points in history
+  console.log(history[0].length)
   graphLayer.noStroke();
-  graphLayer.ellipse(x, y, 5, 5);  // Draw the point for the current population and growth
-  // graphLayer.blendMode(BLEND)
+
+  //population, growth graph
+  for (let i = history[0].length - 1; i >= 0; i--) {
+    // Map the population and growth to the graph's coordinates
+    let x = map(history[0][i], 0, cols * rows * 4, 10, 410);  // Adjust the divisor to fit the graph on the screen
+    let y = map(history[5][i], -200, 200, 380, 80);  // Adjust the divisor and invert y-axis
+    graphLayer.fill(255, history[0].length - i, 0, (i - history[0].length) + 255  );
+    graphLayer.ellipse(x, y, 5, 5);  // Draw the point for the current population and growth
+}
+  //heart beat graph
+  graphLayer.fill(0, 0, 255);
+
+  for (let i = history[0].length - 1; i >= max(0, history[0].length-1000); i--) {
+    let x = 210 + (i - history[0].length)/5;
+    let y = map(history[0][i], 0, cols * rows * 4, 230, 80);
+    graphLayer.circle(x, y, 2);
+  }
+  //add captions to the graphs
+  graphLayer.noStroke();
+  graphLayer.textSize(18);
+  graphLayer.fill(255,0,0);
+  graphLayer.textAlign(LEFT, BOTTOM);
+  graphLayer.text("population", 320, 230);
+  graphLayer.text("growth rate", 15, 80);
+  graphLayer.fill(0,0,255);
+  graphLayer.text("population", 210, 80);
+
 }
 
 function getAge(growth, population, averageRichness, growthRateChange) {
@@ -263,44 +279,28 @@ function getAge(growth, population, averageRichness, growthRateChange) {
   }
 }
 
-// function getAge(growth, population, averageRichness,growthRateChange) {
-//   //ages: eden, pioneer, boom, stagnation, collapse, deathmatch, noah, darkage
-//     if (population == 0) { noLoop(); return "We Extincted. " + bands.length + " bands lived here.";}
-//       else if (growth>0 && averageRichness > 50 && population < totalTerritory/4) return "Age of Eden";
-//       else if ( growth>0 && growthRateChange >0 && population >= totalTerritory/4) return "Age of Pioneer";
-//       else if ( growth>0 && growthRateChange <=0 && population >= totalTerritory *2) return "Age of Boom";
-//       else if ( growth>0 && growthRateChange <=0 && population < totalTerritory *2) return "Age of Stagnation";
-//       }
-//     // }
-//     // if (growth <= -10 && population> totalTerritory*2) return "Age of the Collapse";
-//     // if (growth <= -10 && population<= totalTerritory*2) return "Death Match";
-//     // if (growth >= 10 && population<= totalTerritory/3) return "Age of the Exploitation";
-//     // if (population > totalTerritory*2 && growth >= 10) return "Age of the Exploitation";
-//     // if (population <= totalTerritory/3 && growth < -3) return "Death Match";
-//     // if (population >= totalTerritory && abs(growth)<5) return "Age of the Stagnation";
-//     // if (population <= totalTerritory/5 && averageRichness > 50) return "Age of Eden";
-//     // if (growth > 5) return "Age of Pioneer";
-//     // // return "Dark Age Balance";      
-//     // return "";        
-  
-// // }
+
 
 function displayAgeText(age, alphaValue) {
   topLayer.textSize(90);
   topLayer.textFont("Cormorant");
   topLayer.textAlign(CENTER, CENTER);
   topLayer.colorMode(RGB);
+  topLayer.noStroke();
   topLayer.fill(255, alphaValue);
   topLayer.text(age, width / 2, height / 2);
 }
 
 function restartSimulation() {
+ 
+  isPaused = false;
   let currentSpeed = speedSlider.value();
-  pauseResumeButton.remove();
-  restartButton.remove();
-  speedSlider.remove();
+  pauseResumeButton.html('Pause')
+  pauseResumeButton.remove();  // Remove the old pause/resume button
+  speedSlider.remove();  
   setup();
-  speedSlider.value(currentSpeed);
+  
+  speedSlider.value(currentSpeed); 
   loop();  
 }
 
@@ -310,6 +310,8 @@ function togglePauseResume() {
   if (isPaused) {
 
       pauseResumeButton.html('Resume');  // Update button label to "Resume"
+      noLoop();  // Stop the draw loop
+
   } else {
       pauseResumeButton.html('Pause');  // Update button label to "Pause"
       loop();  // Restart the draw loop if it was stopped
